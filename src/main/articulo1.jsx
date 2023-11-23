0e39ef1a10a1632d20080d2ce4cdd6d1aca03ac9
Knot <aleknot.696@gmail.com>
2023-11-23 11:41:55 -0300

    avances

--- src/main/articulo1.jsx
+++ src/main/articulo1.jsx
@@ -1,8 +1,14 @@
 import './articulo.css'
-import Boton from './boton';
+import { useState } from 'react';
 
 
 function Articulo1({imagen, descripcion, l1, l2, l3, l4, l5, precio}) {
+
+  const [item, setItem] = useState(false);
+
+  const agregar = () => {
+    setItem(true);
+  }
 
   return (
     <div className='article'>
@@ -19,7 +25,8 @@ function Articulo1({imagen, descripcion, l1, l2, l3, l4, l5, precio}) {
         <h3>Valor: $</h3>
         <h3 className='Precio'>{precio}</h3>
       </div>
-      <Boton/>
+      <button id='botonAgregar' onClick={agregar}>Agregar al carrito</button>
+      <h5 className={`pieItem${item ? " pieItem1" : ""}`}>Item agregado al carrito</h5>
     </div>
   )
 }
